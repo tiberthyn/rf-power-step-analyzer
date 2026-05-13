e7435fc610fe30827bbcb78a8a8f64ffad1a1089
tiberthyn <167263992+tiberthyn@users.noreply.github.com>
2026-05-13 16:00:27 -0400

    Add main.h header file with GPIO definitions

---
+++ Core/Inc/main.h
@@ -0,0 +1,88 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32l4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define D0_Pin GPIO_PIN_0
+#define D0_GPIO_Port GPIOC
+#define D1_Pin GPIO_PIN_3
+#define D1_GPIO_Port GPIOF
+#define D2_Pin GPIO_PIN_4
+#define D2_GPIO_Port GPIOF
+#define D3_Pin GPIO_PIN_5
+#define D3_GPIO_Port GPIOF
+#define D4_Pin GPIO_PIN_4
+#define D4_GPIO_Port GPIOE
+#define D5_Pin GPIO_PIN_5
+#define D5_GPIO_Port GPIOE
+
+#define LTC_IN_EN_Pin GPIO_PIN_12
+#define LTC_IN_EN_GPIO_Port GPIOF
+#define LTC_OUT_EN_Pin GPIO_PIN_13
+#define LTC_OUT_EN_GPIO_Port GPIOF
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
